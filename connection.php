f393aac979e8d3806b0b0dbb656a317f74b650dd
Md Atikul Haider <atikul4u@gmail.com>
2022-04-01 14:03:36 +0600

    Add db connection

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+//making the connection with db
+
+$localhost = "localhost";
+$username = "root";
+$password = "";
+$dbname = "sales_system";
+// db connection
+$connect = new mysqli($localhost, $username, $password, $dbname);
+?>
