1b7bd607bffb7c64eecb8caf107282155b6640b7
Md Atikul Haider <atikul4u@gmail.com>
2022-04-01 14:04:28 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+require_once'connection.php';
+
+session_start();
+
+if(isset($_SESSION['userId'])) {
+	header('location: dashboard.php');		
+}
+
+$errors = array();
+
+if($_POST){
+	$user = $_POST['username'];
+	$password = $_POST['password'];
+	if(empty($user) || empty($password)) {
+		if($user == "") {
+			$errors[] = "Username is required";
+		} 
+
+		if($password == "") {
+			$errors[] = "Password is required";
+		}
+	} else {
+		$sql = "SELECT * FROM sales_manager WHERE salesman_id = '$user'";
+		$result = $connect->query($sql);
+
+		if($result->num_rows == 1) {
+			$password = md5($password);
+			$mainSql = "SELECT * FROM sales_manager WHERE salesman_id = '$user' AND salesman_pass = '$password'";
+			$mainResult = $connect->query($mainSql);
+
+			if($mainResult->num_rows == 1) {
+				$value = $mainResult->fetch_assoc();
+				$user_id = $value['salesman_id'];
+				$_SESSION['userId'] = $user_id;
+
+				header('location:'.$store_url.'dashboard.php');	
+			} else{
+				
+				$errors[] = "Incorrect username/password combination";
+			}
+		} else {		
+			$errors[] = "Username doesnot exists";		
+		}
+	}
+}
+
+?>
