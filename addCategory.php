c598942d29edbcad3d8fa8100d90d303de533abe
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-02 13:38:07 +0600

    Update addCategory.php

--- addCategory.php
+++ addCategory.php
@@ -4,3 +4,8 @@ require_once 'connection.php';
 
 $valid['success'] = array('success' => false, 'messages' => array());
 
+if($_POST) {
+	$categoriesName = $_POST['categoriesName'];
+	$categoriesStatus = $_POST['categoriesStatus'];
+
+}
