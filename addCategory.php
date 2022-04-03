9fa44e2b421b66b4536fda5102e80a6251ac6dd4
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-03 13:39:19 +0600

    Update addCategory.php

--- addCategory.php
+++ addCategory.php
@@ -8,4 +8,18 @@ if($_POST) {
 	$categoriesName = $_POST['categoriesName'];
 	$categoriesStatus = $_POST['categoriesStatus'];
 
+	$sql = "INSERT INTO category (category_name, category_status) 
+			VALUES ('$categoriesName', '$categoriesStatus')";
+
+	if($connect->query($sql) === TRUE) {
+		$valid['success'] = true;
+		$valid['messages'] = "Successfully Added";	
+	} else {
+		$valid['success'] = false;
+		$valid['messages'] = "Error";
+	}
+
+	$connect->close();
+
+	echo json_encode($valid);
 }
