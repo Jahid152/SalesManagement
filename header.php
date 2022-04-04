a2e810c3acf065ee354eb584882dffcefdd59ef8
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:17:09 +0600

    Update header.php

--- header.php
+++ header.php
@@ -20,6 +20,16 @@ require_once'connection.php';
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
+	  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+	    	<li class="nav-item active">
+	    		<a class="nav-link" href="dashboard.php"><i class="fa fa-tachometer"></i> Dashboard <span class="sr-only">(current)</span></a>
+	    	</li>
+	    	<li class="nav-item">
+		        <a class="nav-link" href="category.php"><i class="fa fa-tasks"></i> Category</a>
+		    </li>
+	    </ul>
+	  </div>
 	</nav>	 
 
 	<div class="container">
