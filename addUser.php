f2ad1925d35da9e753a419c3af6bf18aa8a114e5
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:24:02 +0600

    Update addUser.php

--- addUser.php
+++ addUser.php
@@ -5,5 +5,11 @@ require_once 'connection.php';
 $valid['success'] = array('success' => false, 'messages' => array());
 
 if($_POST) {
+	$userName = $_POST['userName'];
+	$userFullName = $_POST['userFullName'];
+	$contactNo = $_POST['contactNo'];
+	$emailAddress = $_POST['emailAddress'];
+	$userPassword = md5($_POST['userPassword']);
+
 	
 }
