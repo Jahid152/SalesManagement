b25e25f93c28dfaf5e402f5a72210a5334e302cf
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:16:23 +0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php 
+require_once'connection.php';
+?>
+<html>
+<head>
+	<link rel="stylesheet" href="file/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="file/bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="file/bootstrap/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="file/font-awesome/css/font-awesome.css">
+	
+	<script src="file/jquery/jquery.min.js"></script>
+	  <!-- jquery ui -->  
+    <link rel="stylesheet" href="file/jquery-ui/jquery-ui.min.css">
+    <script src="file/jquery-ui/jquery-ui.min.js"></script>
+    
+	<script src="file/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+
+	<div class="container">
