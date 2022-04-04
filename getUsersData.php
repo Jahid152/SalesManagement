ef6f7b8f91fa558588ef108977bb31c29f817419
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:22:58 +0600

    Update getUsersData.php

--- getUsersData.php
+++ getUsersData.php
@@ -2,4 +2,6 @@
 header("Content-Type: application/json; charset=UTF-8");
 require_once'connection.php';
 
+$sql = "SELECT salesman_id, salesman_name, salesman_contact, salesman_email	FROM sales_manager";
+
 ?>
