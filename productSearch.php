385c0571c4674c001e0d6cfd823ba94b1696abf8
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:25:50 +0600

    Update productSearch.php

--- productSearch.php
+++ productSearch.php
@@ -3,5 +3,9 @@ header("Content-Type: application/json; charset=UTF-8");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$searchByName = $_POST["searchByName"];
 }
+require_once'connection.php';
+
+$sql = "SELECT product_image, product_id, product_name, product_price, product_qty, product_status	FROM product
+		 		WHERE product_qty>0 AND product_name LIKE '%$searchByName%'";
 
 ?>
