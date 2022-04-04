cfaaef8c1cfb1c00a49c3a3061550f28bcdc652e
Md Atikul Haider <atikul4u@gmail.com>
2022-04-04 14:26:03 +0600

    Update productSearch.php

--- productSearch.php
+++ productSearch.php
@@ -8,4 +8,5 @@ require_once'connection.php';
 $sql = "SELECT product_image, product_id, product_name, product_price, product_qty, product_status	FROM product
 		 		WHERE product_qty>0 AND product_name LIKE '%$searchByName%'";
 
+echo json_encode($outp);
 ?>
