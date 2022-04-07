8ae2f7f95cea3122a873e895c8b071980fd8461c
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-07 13:52:00 +0600

    Update getCategoryData.php

--- getCategoryData.php
+++ getCategoryData.php
@@ -4,4 +4,5 @@ header("Content-Type: application/json; charset=UTF-8");
 require_once'connection.php';
 $sql = "SELECT category.category_id, category.category_name, category.category_status FROM category
 		 		WHERE category.category_status = 1";
+$stmt = $connect->prepare($sql);
 ?>
