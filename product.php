5a2873a972412476d57edc55aaad66aa8c7b6317
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-11 14:00:29 +0600

    Create product.php

---
+++ product.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'header.php';
+?>
+
+	<div class="row">
+		<div class="col-md-12">
+
+			<div class="panel panel-default border rounded">
+				<div class="panel-heading">
+					<div class="heading" style="padding: 5px;"> Manage Product</div>
+				</div>
+				<div class="panel-body">
+					
+					<div class="remove-messages"></div>
+					
+					<div class="div-action pull pull-left" style="padding:20px;">
+						<button class="btn btn-default button1" data-toggle="modal" id="addProductModalBtn" data-target="#addProductModal">
+							<i class="fa fa-plus"></i> Add Product
+						</button>
+					</div>	
+					
+</body>
+</html>
