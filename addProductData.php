59a50b9e70af99202b8f4c18e67270679ff376aa
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-13 14:15:39 +0600

    Update addProductData.php

--- addProductData.php
+++ addProductData.php
@@ -18,13 +18,24 @@ if($_POST) {
 		if(is_uploaded_file($_FILES['productImage']['tmp_name'])) {			
 			if(move_uploaded_file($_FILES['productImage']['tmp_name'], $imageLocation)) {
 				
-				
+				$sql = "INSERT INTO product (product_image, product_name, product_price, product_qty, product_status) 
+				VALUES ('$imgName', '$productName', '$rate', '$quantity', '$productStatus')";
+
+				if($connect->query($sql) === TRUE) {
+					$valid['success'] = true;
+					$valid['messages'] = "Successfully Added";	
+				} else {
+					$valid['success'] = false;
+					$valid['messages'] = "Error";
+				}
 
 			}	else {
 				return false;
-			}		
+			}	
 		}
 	}		
 
-	
+	$connect->close();
+
+	echo json_encode($valid);
 }
