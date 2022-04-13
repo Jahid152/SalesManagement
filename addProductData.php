271c2309c55387076857cf5e84fd973d557410b0
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-04-13 14:13:12 +0600

    Update addProductData.php

--- addProductData.php
+++ addProductData.php
@@ -4,5 +4,14 @@ require_once 'connection.php';
 
 $valid['success'] = array('success' => false, 'messages' => array());
 
+if($_POST) {
+	$productName = $_POST['productName'];
+	$rate = $_POST['price'];
+ 	$quantity= $_POST['quantity'];	
+	$productStatus 	= $_POST['productStatus'];
 
+	
+	}		
+
+	
 }
