8f2738d1fe7622d15f908731e2d1998032a41b77
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-05-21 13:52:55 +0600

    Update addCategory.php

--- addCategory.php
+++ addCategory.php
@@ -3,3 +3,10 @@ header("Content-Type: application/json; charset=UTF-8");
 require_once 'connection.php';
 
 $valid['success'] = array('success' => false, 'messages' => array());
+
+if($_POST) {
+	$categoriesName = $_POST['categoriesName'];
+	$categoriesStatus = $_POST['categoriesStatus'];
+
+	echo json_encode($valid);
+}
