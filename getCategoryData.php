4e7bad35c93a7d73dcd63302ee127425e2de9bb5
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-05-21 13:51:26 +0600

    Update getCategoryData.php

--- getCategoryData.php
+++ getCategoryData.php
@@ -6,3 +6,8 @@ $sql = "SELECT category.category_id, category.category_name, category.category_s
 		 		WHERE category.category_status = 1";
 $stmt = $connect->prepare($sql);
 $stmt->execute();
+$result = $stmt->get_result();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
+?>
