c9f11af4dbdb87fb7520f6980b2555f6dc933ff3
Jahid152 <mislam152212@bscse.uiu.ac.bd>
2022-05-21 13:55:09 +0600

    Update delCategory.php

--- delCategory.php
+++ delCategory.php
@@ -4,3 +4,10 @@ require_once 'connection.php';
 
 $valid['success'] = array('success' => false, 'messages' => array());
 
+if($_POST) {
+	$catid = $_POST['catid'];
+
+	$sql = "DELETE FROM category WHERE category_id = $catid";
+
+
+}
